bcd9b0ed2aea0f58500a5a901b525b1edc5f25bb
Elliot Vasquez <emvasquez@usf.edu>
2020-10-05 21:51:42 -0400

    V1

--- HelloWorld1/Program.cs
+++ HelloWorld1/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿//Author: Elliot Vasquez
+// ISM 4212 Assignment #1
+//Comment: This C# console will demonstrate the execution of the WriteLine command to display "Hello World!".  
 
-namespace HelloWorld1
+
+using System;
+
+namespace HelloWorldd
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //A line that lists > Hello World < will appear.
+
+            Console.ReadKey(true);
         }
     }
 }
